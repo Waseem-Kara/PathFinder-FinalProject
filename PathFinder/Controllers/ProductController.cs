171ec69c37404e4c06e09e4e38fec6e193349b48
Waseem Kara <was_k@hotmail.co.uk>
2017-03-27 11:53:29 +0100

    Added Product controller with views

---
+++ PathFinder/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PathFinder.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View("Keyboard");
+        }
+
+        public ActionResult Accessories()
+        {
+            return View("Accessories");
+        }
+
+        public ActionResult Software()
+        {
+            return View("Software");
+        }
+    }
+}
