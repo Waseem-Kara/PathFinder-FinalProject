8ae60cd558d6f06941f200425fe9d92bd2e666cf
Waseem Kara <was_k@hotmail.co.uk>
2017-04-12 15:51:08 +0100

    Removed local db

--- PathFinder/App_Data/dbo.Table.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[Table]
-(
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [Title] NCHAR(10) NOT NULL, 
-    [Firstname] NCHAR(10) NOT NULL, 
-    [Surname] NCHAR(10) NOT NULL, 
-    [Email] NCHAR(10) NOT NULL, 
-    [Phone] NCHAR(10) NULL
-)
