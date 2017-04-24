013af31edf5ba20f3c9ac6e173473c7fb675fdb4
Waseem Kara <was_k@hotmail.co.uk>
2017-04-24 14:00:45 +0100

    added unitest project, sweetalert, buffer screen

---
+++ PathFinderTest/PathFinderUnitTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PathFinder.Controllers;
+using NUnit.Framework;
+using System.Web.Mvc;
+
+namespace PathFinderTest
+{
+    [TestClass]
+    public class PathFinderUnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Arrange
+            AccountController acController = new AccountController();
+
+            //Act
+            var actual = acController.Login();
+
+            //Assert
+            Assert.IsInstanceOf<ActionResult>(actual);
+        }
+    }
+}
+    
